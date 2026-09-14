5bf33d9637635051ced73105b0335577bd20c813
armandogonzalez-gif <armando.gonzalez@pricetravel.com>
2026-09-14 15:52:45 -0600

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-playwright install chromium
+set -eux
+
+# Instalar dependencias de Python
+pip install -r requirements.txt
+
+# Instalar navegadores de Playwright dentro del entorno de Python
+python -m playwright install --with-deps chromium
